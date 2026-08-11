9b78b939b1b6c235de1de14bb95cee640393637a
azlaof <joan.rodriguezc@upb.edu.co>
2026-08-11 09:40:45 -0500

    Diagrama de flujo codigo

---
+++ diagrama de flujo/diagrama de flujo/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace diagrama_de_flujo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0;
+            int num2 = 0;
+            int num3 = 0;
+            Console.WriteLine("Debe ingresar 3 valores diferentes");
+            Console.WriteLine("Ingrese el valor del numero 1");
+            num1=int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el valor del numero 2");
+            num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el valor del numero 3");
+            num3 = int.Parse(Console.ReadLine());
+
+            if (num1 > num2)
+            {
+                if (num1 > num3)
+                {
+                    Console.WriteLine("El numero 1 es mayor");
+                }
+                else
+                {
+                    Console.WriteLine("El numero 3 es mayor");
+                }
+
+            }
+            else
+            {
+                if (num2 > num1)
+                {
+                    Console.WriteLine("El numero 2 es mayor");
+                }
+                else
+                {
+                    Console.WriteLine("el numero 3 es mayor");
+                }
+
+            }
+
+
+        }
+    }
+}
