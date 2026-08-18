8a9f335a69c143539a3889d67648740babb99a41
azlaof <joan.rodriguezc@upb.edu.co>
2026-08-18 09:36:21 -0500

    Taller de preparación

---
+++ Taller/Taller/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace Taller
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {  /* EJERCICIO 1
+            int numero1 = 0;
+            int numero2 = 0;
+            int numero3 = 0;
+            Console.WriteLine("Ingrese 3 número diferentes");
+            Console.WriteLine("Ingrese el primer número");
+            numero1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el segundo número");
+            numero2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el tercer número");
+            numero3 = int.Parse(Console.ReadLine());
+
+            if (numero1 != numero2 && numero1 != numero3 && numero2 != numero3)
+            {
+                if (numero1 > numero2)
+                {
+                    if (numero1 > numero3)
+                    {
+                        Console.WriteLine("El número 1 es el mayor");
+                        if (numero3 > numero2)
+                        {
+                            Console.WriteLine("El orden de menor a mayor son:");
+                            Console.WriteLine(numero2);
+                            Console.WriteLine(numero3);
+                            Console.WriteLine(numero1);
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("El orden de menor a mayor son:");
+                            Console.WriteLine(numero3);
+                            Console.WriteLine(numero2);
+                            Console.WriteLine(numero1);
+
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("El número 3 es el mayor");
+                        Console.WriteLine("El orden de menor a mayor son:");
+                        Console.WriteLine(numero2);
+                        Console.WriteLine(numero1);
+                        Console.WriteLine(numero3);
+                    }
+                }
+                else
+                {
+                    if (numero2 > numero3)
+                    {
+                        Console.WriteLine("El número 2 es el mayor");
+                        if (numero1 > numero3)
+                        {
+                            Console.WriteLine("El orden de menor a mayor son:");
+                            Console.WriteLine(numero3);
+                            Console.WriteLine(numero1);
+                            Console.WriteLine(numero2);
+                        }
+                        else
+                        {
+                            Console.WriteLine("El orden de menor a mayor son:");
+                            Console.WriteLine(numero1);
+                            Console.WriteLine(numero3);
+                            Console.WriteLine(numero2);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("El número 3 es el mayor");
+                        Console.WriteLine("El orden de menor a mayor es:");
+                        Console.WriteLine(numero1);
+                        Console.WriteLine(numero2);
+                        Console.WriteLine(numero3);
+
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Todos deben ser valores diferentes");
+            }*/
+        }
+    }
+}
